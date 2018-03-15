ce70e85a8b73957592bcb3ffc83ef64a6f5e018d
Esty Katz <stk@Estys-MacBook-Pro.local>
2018-03-15 11:37:52 +0200

    a working js project

--- index.js
+++
@@ -1,3 +0,0 @@
-const padLeft = require('@bit/ranm8.walmart.string.pad-left');
-console.log(padLeft(null, 5, '0'));
-
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import padLeft from './pad-left';
+console.log(padLeft("hi", 5, '0'));
+
